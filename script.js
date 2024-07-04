5d409d361d8dc0fd75fee5ac179d8a4e2cfca5a9
frederickcorredor <144303183+frederickcorredor@users.noreply.github.com>
2024-07-03 20:35:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function girar1(){
+    document.getElementById('adrian1').style.display='none';
+    document.getElementById('adrian2').style.display='block';
+}
+
+function girar2(){
+    document.getElementById('david1').style.display='none';
+    document.getElementById('david2').style.display='block';
+}
+
+function girar3(){
+    document.getElementById('fred1').style.display='none';
+    document.getElementById('fred2').style.display='block';
+}
+
+function regresar1(){
+    document.getElementById('adrian1').style.display='block';
+    document.getElementById('adrian2').style.display='none';
+}
+
+function regresar2(){
+    document.getElementById('david1').style.display='block';
+    document.getElementById('david2').style.display='none';
+}
+
+function regresar3(){
+    document.getElementById('fred1').style.display='block';
+    document.getElementById('fred2').style.display='none';
+}
